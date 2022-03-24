a0b284ff339ef7cea027e2656b8db78f60120ce4
filip-we <filip.wettlen@gmail.com>
2022-03-24 21:37:18 +0100

    We have a font packed in a nice bit-format!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # terminal
+
+The aim of this project is to build a terminal that can be used in conjuction with a Linux PC. It includes both custom hardware and software. The physical components are a RaspberryPi Pico as the controller and a 2.4 inch TFT-screen. Coding will need to inculde the major components of reading from a USB-keyboard, keeping track of a screen buffer, reacting on IO from the host PC and of course print to the TFT-screen.
--- src/font/parse_png.py
+++ src/font/parse_png.py
@@ -1,43 +1,51 @@
 from PIL import Image
 from sys import argv, exit
 
-def get_graphical_string(image_name, check=(255, 255, 255)):
+def convert_image_to_bitmap(image_name, check=(255, 255, 255), char_dim=(8, 16)):
     im = Image.open(image_name, 'r')
     width, height = im.size
-    pixels = list(im.getdata())
-    s = ''
-    for h in range(height):
-        for w in range(width):
-            p = pixels[w + h * width]
-            s += chr(0x25A0) if p == check else ' '
-        s+= '\n'
-    return s
-
-def convert_image_to_bytemap(image_name, check=(255, 255, 255)):
-    im = Image.open(image_name, 'r')
-    width, height = im.size
+    nr_chars_per_row = int(width / char_dim[0])
+    nr_chars_per_col = int(height / char_dim[1])
     pixels = list(im.getdata())
     all_bytes = list()
-    current_byte = 0
+    current_char = 0
     n = 0
-    for h in range(height):
-        for w in range(width):
-            p = pixels[w + h * width]
-            current_byte = (current_byte << 1) + int(p == check)
-            n += 1
-            if n == 8:
-                all_bytes.append(current_byte)
-                current_byte = 0
-                n = 0
+    for char_y in range(nr_chars_per_col):
+        for char_x in range(nr_chars_per_row):
+            for h in range(char_dim[1]): # It feels most natural to scan from left to right.
+                offset = width * h + char_dim[0] * char_x + char_y * char_dim[0] * char_dim[1]
+                for w in range(char_dim[0]):
+                    p = pixels[offset + w]
+                    current_char = (current_char << 1) + int(p == check)
+                    n += 1
+                    if n == 8:
+                        all_bytes.append(current_char)
+                        current_char = 0
+                        n = 0
     return bytearray(all_bytes)
+
+def print_char_from_bitmap(file_name, char_nrs, char_dim=(8, 16), image_char_map=(8, 8)):
+    area_bits = char_dim[0] * char_dim[1]
+    area_bytes = int(char_dim[0] * char_dim[1] / 8)
+    s = ''
+    with open(file_name, 'rb') as fb:
+        for c in char_nrs:
+            fb.seek(c * area_bytes)
+            data = fb.read(area_bytes)
+            print(data)
+            for b in data:
+                for i in range(8):
+                    s += chr(0x25A0) if (b >> i) & 1 else ' '
+                s += '\n'
+    return s
 
 if __name__ == '__main__':
     if len(argv) < 2:
         print('Please supply an image!')
         exit(1)
 
-    all_bytes = convert_image_to_bytemap(argv[1])
-    with open(argv[1] + '.bin', 'wb') as fb:
-        fb.write(all_bytes)
+    file_name = '.'.join(argv[1].split('.')[:-1])
+    with open(file_name + '.bin', 'wb') as fb:
+        fb.truncate()
+        fb.write(convert_image_to_bitmap(argv[1]))
 
-    print(get_graphical_string(argv[1]))
