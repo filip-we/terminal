1bfb18d0fc5e7aa2ae85bc916e7ca9da588d6cbc
filip-we <filip.wettlen@gmail.com>
2022-09-09 22:15:58 +0200

    Sending keystrokes to UART, printing UART RX to screen.

--- src/main.c
+++ src/main.c
@@ -8,20 +8,36 @@
 
 #include "pico/stdlib.h"
 #include "bsp/board.h"
+#include "hardware/uart.h"
+#include "hardware/irq.h"
 #include "tusb.h"
+
 #include "screen.c"
 
-static void start_communications();
-
+void on_uart_rx() {
+    while (uart_is_readable(TERM_UART)) {
+        uint8_t ch = uart_getc(TERM_UART);
+        screen_write_char_at_cursor(ch);
+    }
+}
 
 static void start_communications()
 {
-    stdio_init_all();
-    gpio_set_function(PICO_DEFAULT_UART_TX_PIN , GPIO_FUNC_UART);
-    gpio_set_function(PICO_DEFAULT_UART_RX_PIN , GPIO_FUNC_UART);
-    gpio_set_dir(PICO_DEFAULT_UART_TX_PIN, GPIO_OUT);
-    gpio_set_dir(PICO_DEFAULT_UART_RX_PIN, GPIO_IN);
+    //stdio_init_all();
+    //gpio_set_function(PICO_DEFAULT_UART_TX_PIN , GPIO_FUNC_UART);
+    //gpio_set_function(PICO_DEFAULT_UART_RX_PIN , GPIO_FUNC_UART);
+    //gpio_set_dir(PICO_DEFAULT_UART_TX_PIN, GPIO_OUT);
+    //gpio_set_dir(PICO_DEFAULT_UART_RX_PIN, GPIO_IN);
 
+    uart_init(TERM_UART, TERM_UART_BAUDRATE);
+    gpio_set_function(TERM_UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(TERM_UART_RX_PIN, GPIO_FUNC_UART);
+    gpio_set_dir(TERM_UART_TX_PIN, GPIO_OUT);
+    gpio_set_dir(TERM_UART_RX_PIN, GPIO_IN);
+
+    uart_set_hw_flow(TERM_UART, false, false);
+    uart_set_format(TERM_UART, 8, 1, UART_PARITY_NONE);
+    uart_set_fifo_enabled(TERM_UART, false);
 }
 
 
@@ -41,10 +57,11 @@ int main (void)
     {
         tuh_task();
         screen_update();
+        on_uart_rx();
 
-        if (kbd_buff_read != kbd_buff_write)
+        if (kbd_buff_read != kbd_buff_write && uart_is_writable(TERM_UART))
         {
-            printf("%c", kbd_buff[kbd_buff_read]);
+            uart_putc(TERM_UART, kbd_buff[kbd_buff_read]);
             kbd_buff_read++;
         }
     }
