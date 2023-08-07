2c1cb5e304bb0829ad27f3d7988df9d82ed990a4
filip-we <filip.wettlen@gmail.com>
2023-08-07 22:38:37 +0200

    Added test for carrige return.

--- tests/test.py
+++ tests/test.py
@@ -3,12 +3,26 @@ import serial
 
 BAUDRATE = 115200
 
+class ExtendedSerial(serial.Serial):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def write_ascii(self, string):
+        self.write(string.encode("ascii"))
+
+
 @pytest.fixture
 def con(port):
-    return serial.Serial(
+    return ExtendedSerial(
         port=port,
         baudrate=BAUDRATE,
     )
 
 def test_hello_world(con):
     con.write("Hello, world".encode("ascii"))
+
+def test_carrige_return(con):
+    con.write_ascii("Row1")
+    con.write(b'\r')
+    con.write_ascii("Row2")
+    con.write(b'\r')
