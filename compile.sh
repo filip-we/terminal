afba9269f4e082d2b70b0c5e318ce0972f0e4b81
filip-we <filip.wettlen@gmail.com>
2023-08-12 22:59:19 +0200

    RP-firmware compiling and working on the hardware.

--- compile.sh
+++ compile.sh
@@ -8,14 +8,14 @@ cd build
 make
 
 cd ..
-#read -p "Upload to "$1"?" -n 1 -r
-#echo    # (optional) move to a new line
-#if [[ $REPLY =~ ^[Yy]$ ]]
-#then
-#    cp build/terminal.uf2 $1
-#fi
+read -p "Upload to "$1"?" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cp build/rp_pico_program/rp_pico_program.uf2 $1
+fi
 
 #openocd -f interface/raspberrypi-swd.cfg \
 #-f target/rp2040.cfg \
-#-c "program build/terminal.elf verify reset exit"
+#-c "program build/rp_pico_program/rp_pico_program.elf verify reset exit"
 
