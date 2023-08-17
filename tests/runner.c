f6cae7553a8c3415645b7fa453f3c61e035d44a8
filip-we <filip.wettlen@gmail.com>
2023-08-17 22:25:10 +0200

    WIP adding test

--- tests/runner.c
+++ tests/runner.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include "parser.h"
+//#include "screen.h"
 
 int tests_run = 0;
 
-#define FAIL() printf("\nfailure in %s() line %d\n", __func__, __LINE__)
+#define FAIL() printf("\n %s() failed @ line %d\n", __func__, __LINE__)
 #define _assert(test) do { if (!(test)) { FAIL(); return 1; } } while(0)
 #define _verify(test) do { int r=test(); tests_run++; if(r) return r; } while(0)
+
+int test_hello_world()
+{
+    uint8_t scroll = 0;
+    parse_byte('H',
+        &cursor,
+        *screen_buffer,
+        &scroll);
+    _assert( (&screen_buffer) == 'H');
+}
 
 int square_01() {
     int x=5;
